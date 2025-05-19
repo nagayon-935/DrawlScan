196fbc66890b5d806b5d07f48f06029680acb69e
nagayon-935 <92247537+nagayon-935@users.noreply.github.com>
2025-05-20 00:06:41 +0900

    add: option handler

--- cmd/handler/option_handler.go
+++ cmd/handler/option_handler.go
@@ -73,25 +73,20 @@ func buildFlagSet() (*flag.FlagSet, *options) {
 	flags := flag.NewFlagSet("drawlscan", flag.ContinueOnError)
 	flags.Usage = func() { fmt.Println(helpMessage()) }
 
-	// Analysis options
 	flags.BoolVarP(&opts.Analysis.Geoip, "geoip", "g", false, "Show GeoIP information for source and destination IP addresses")
 	flags.BoolVarP(&opts.Analysis.Rdns, "rdns", "r", false, "Perform reverse DNS lookups on source and destination IP addresses")
 	flags.StringVarP(&opts.Analysis.Protocol, "protocol", "p", "all", "Filter packets by protocol (e.g., TCP, UDP, ICMP)")
 	flags.StringVarP(&opts.Analysis.Port, "port", "P", "all", "Filter packets by port number (e.g., 80, 443)")
 
-	// Capture options
 	flags.IntVarP(&opts.Capture.Count, "count", "c", 0, "Capture only a specified number of packets")
 	flags.IntVarP(&opts.Capture.Timeout, "timeout", "t", 0, "Stop capturing after a specified number of seconds")
 
-	// General options
 	flags.BoolVarP(&opts.General.Help, "help", "h", false, "Help message")
 	flags.BoolVarP(&opts.General.Version, "version", "v", false, "Version information")
 
-	// IO options
 	flags.StringVarP(&opts.Io.InterfaceName, "interface", "i", "", "Specify the network interface to capture packets from (e.g., eth0, wlan0)")
 	flags.StringVarP(&opts.Io.OutputFile, "output", "o", "", " Save the captured packets to a file in PCAP format")
 
-	// Visualization options
 	flags.BoolVar(&opts.Visualization.Ascii, "ascii", true, "Enable ASCII-art visualization of packets and traffic (Default is enable)")
 	flags.BoolVar(&opts.Visualization.NoAscii, "no-ascii", false, "Disable ASCII-art output")
 
@@ -116,7 +111,6 @@ func collectFieldMap(value reflect.Value, optionMap map[string]interface{}) {
 	}
 }
 
-// 使い方例
 func OptionHandler(optArgs []string) map[string]interface{} {
 	flags, options := buildFlagSet()
 	flags.Parse(optArgs[1:])
