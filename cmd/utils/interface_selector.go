a12bf637fe44caeb9df538c052800760348c4874
nagayon-935 <92247537+nagayon-935@users.noreply.github.com>
2025-05-30 12:18:14 +0900

    add: interface select

---
+++ cmd/utils/interface_selector.go
@@ -0,0 +1,52 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sort"
+	"strings"
+
+	"github.com/google/gopacket/pcap"
+)
+
+func AutoSelectInterface() string {
+	ifs, err := net.Interfaces()
+	if err != nil {
+		log.Fatal("Failed to get interfaces:", err)
+	}
+
+	// インターフェイスをインデックスの昇順でソート
+	sort.Slice(ifs, func(i, j int) bool {
+		return ifs[i].Index < ifs[j].Index
+	})
+
+	for _, iface := range ifs {
+
+		if (iface.Flags&net.FlagUp != 0) && (iface.Flags&net.FlagLoopback == 0) && !strings.HasPrefix(iface.Name, "utun") {
+			if isInterfaceConnected(iface.Name) {
+				return iface.Name
+			}
+		}
+	}
+
+	fmt.Println("No suitable interface found.")
+	return ""
+}
+
+func isInterfaceConnected(ifaceName string) bool {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		return false
+	}
+
+	for _, dev := range devices {
+		if dev.Name == ifaceName {
+			if len(dev.Addresses) > 0 {
+				return true
+			}
+		}
+	}
+
+	return false
+}
